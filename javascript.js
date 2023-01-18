06079c48940cb2dbccc1da9a5f1eeb4b7c50f3e0
Hamza Iftikhar <thehamzaiftikhar@gmail.com>
2023-01-17 22:49:57 -0500

    Card Created

---
+++ javascript.js
@@ -0,0 +1,7 @@
+const btn = document.querySelector("#button");
+btn.addEventListener("click", link);
+
+function link() {
+    location.href = "https://www.instagram.com/brickstobooks/";
+    console.log("click");
+}
